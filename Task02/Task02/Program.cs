07c72cbbceb27bd05526d1ddcf0357723666f7d1
E.P.Berdysheva@urfu.me <E.P.Berdysheva@urfu.me>
2022-10-06 15:19:36 +0500

    Выполнено задание 2

---
+++ Task02/Task02/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task02
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите сторону куба");
+            double a = double.Parse(Console.ReadLine());
+
+            double v = a * a * a;
+            double diagonal = a * (Math.Sqrt(3));
+
+            Console.WriteLine("Объем куба:" + v);
+            Console.WriteLine("Диагональ куба:"+ diagonal);
+            Console.ReadKey();
+        }
+    }
+}
