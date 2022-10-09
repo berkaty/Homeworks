84cb6a4eaac8a24c301222629d8559902b3ea968
E.P.Berdysheva@urfu.me <E.P.Berdysheva@urfu.me>
2022-10-09 17:27:36 +0500

    Выполнено задание 3

---
+++ Task03/Task03/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task03
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите час");
+            double hour = double.Parse(Console.ReadLine());
+            Console.WriteLine("Введите минуту");
+            double minute = double.Parse(Console.ReadLine());
+
+            if (hour == minute)
+            {
+                Console.WriteLine(minute);
+            }
+            else if ( hour > minute)
+            {
+                Console.WriteLine(hour - minute);
+            }
+            else if (hour < minute)
+            {
+                Console.WriteLine(60 - minute + hour + 1);
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
