e443a9a5335f8f6fa132d66da894ac210659b88e
E.P.Berdysheva@urfu.me <E.P.Berdysheva@urfu.me>
2022-10-13 22:32:11 +0500

    Выполнено задание 4

---
+++ Task04/Task04/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task04
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите x");
+            double x = double.Parse(Console.ReadLine());
+            double y = Math.Sqrt( ( 2*x + Math.Sin(Math.Abs(3*x* Math.PI / 180)) ) / 3.56);
+
+            Console.WriteLine("y=" + y);
+            Console.ReadKey();
+        }
+    }
+}
