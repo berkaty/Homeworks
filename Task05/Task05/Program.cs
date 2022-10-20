51c4557aea923165b60b56b8459f5883c27b3754
E.P.Berdysheva@urfu.me <E.P.Berdysheva@urfu.me>
2022-10-20 23:37:08 +0500

    Задание 5 выполнено

---
+++ Task05/Task05/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task05
+{
+    internal class Program
+    {
+        public static void Main()
+        {
+            Console.WriteLine(Math.Round( Square(2 + Square(3 + Square(4 + Square(5)))),3));
+        }
+
+        public static double Square(double x)
+        {
+            return Math.Sqrt(x);
+        }
+    }
+}
