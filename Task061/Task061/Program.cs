d934a0fe17d9712d76e1b7c406517ded92b5a05c
E.P.Berdysheva@urfu.me <E.P.Berdysheva@urfu.me>
2022-10-30 23:19:36 +0500

    Задание 6.2 выполнено

---
+++ Task061/Task061/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task061
+{
+    internal class Program
+    {
+        public static void Main()
+        {
+            Console.WriteLine("Введите текст: ");
+            string text = Console.ReadLine();
+        }
+
+        public static string Alph(string text)
+        {
+                string alphB = "АБВГДЕЁЖЗИЙКЛМНОПРСТУФХЦЧШЩЪЫЬЭЮЯ";
+                string alphs = "абвгдеёжзийклмнопрстуфхцчшщъыьэюя";
+                string CS = "A6BrDEE";
+                for i in range (33):
+
+                return &&&;
+        }
+
+        
+    }
+}
+////"А","Б","В","Г","Д","Е","Ё","Ж","З","И","Й","К","Л","М","Н","О","П","Р","С","Т","У","Ф","Х","Ц","Ч","Ш","Щ","Ъ","Ы","Ь","Э","Ю","Я";
---
+++ Task062/Task062/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task062
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            ///Дано слово «трос», получить слова «сорт» и «рост».
+            string a = "трос";
+            Console.WriteLine(a.Substring(3, 1) + a.Substring(2, 1) + a.Substring(1, 1) + a.Substring(0, 1));
+            Console.WriteLine(a.Substring(1, 3) + a.Substring(0, 1));
+
+            Console.ReadKey();
+        }
+    }
+}
