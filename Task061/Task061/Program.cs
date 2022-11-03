69b9d61d49f646bb6baaae08e22804c3692a17e9
E.P.Berdysheva@urfu.me <E.P.Berdysheva@urfu.me>
2022-11-03 21:30:42 +0500

    Задание 6.1 выполнено

--- Task061/Task061/Program.cs
+++ Task061/Task061/Program.cs
@@ -1,30 +1,67 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using static System.Net.Mime.MediaTypeNames;
 
 namespace Task061
 {
     internal class Program
     {
-        public static void Main()
+        public static void Main(string[] args)
+        {
+
+            Console.WriteLine(Replacement());
+
+        }
+        public static string Input()
         {
             Console.WriteLine("Введите текст: ");
-            string text = Console.ReadLine();
+            string text=Console.ReadLine();
+            text=text.ToUpper();
+            return text;
+            
+            
         }
 
-        public static string Alph(string text)
+        public static string Replacement()
         {
-                string alphB = "АБВГДЕЁЖЗИЙКЛМНОПРСТУФХЦЧШЩЪЫЬЭЮЯ";
-                string alphs = "абвгдеёжзийклмнопрстуфхцчшщъыьэюя";
-                string CS = "A6BrDEE";
-                for i in range (33):
 
-                return &&&;
+            string text1 = Input();
+            text1=text1.Replace('А', 'A');
+            text1 = text1.Replace('Б', '6');
+            text1 = text1.Replace("В", "B");
+            text1 = text1.Replace("Г", "r");
+            text1 = text1.Replace("Д", "r");
+            text1 = text1.Replace("Е", "E");
+            text1 = text1.Replace("Ё", "E");
+            text1 = text1.Replace("Ж", "}|{");
+            text1 = text1.Replace("З", "3");
+            text1 = text1.Replace("И", "u");
+            text1 = text1.Replace("Й", "u*");
+            text1 = text1.Replace("К", "K");
+            text1 = text1.Replace("Л", "JI");
+            text1 = text1.Replace("М", "M");
+            text1 = text1.Replace("Н", "H");
+            text1 = text1.Replace("О", "0");
+            text1 = text1.Replace("П", "n");
+            text1 = text1.Replace("Р", "P");
+            text1 = text1.Replace("С", "C");
+            text1 = text1.Replace("Т", "T");
+            text1 = text1.Replace("У", "Y");
+            text1 = text1.Replace("Ф", "cp");
+            text1 = text1.Replace("Х", "X");
+            text1 = text1.Replace("Ц", "L|");
+            text1 = text1.Replace("Ч", "4");
+            text1 = text1.Replace("Ш", "LLI");
+            text1 = text1.Replace("Щ", "LLL");
+            text1 = text1.Replace("Ъ", "'b");
+            text1 = text1.Replace("Ы", "bI");
+            text1 = text1.Replace("Ь", "b");
+            text1 = text1.Replace("Э", "-)");
+            text1 = text1.Replace("Ю", "IO");
+            text1 = text1.Replace("Я", "9I");
+            return text1;
+            
         }
 
         
     }
 }
-////"А","Б","В","Г","Д","Е","Ё","Ж","З","И","Й","К","Л","М","Н","О","П","Р","С","Т","У","Ф","Х","Ц","Ч","Ш","Щ","Ъ","Ы","Ь","Э","Ю","Я";
