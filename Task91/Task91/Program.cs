7df4c545a6f8a9781d6979e9b9a16394a1b1f6df
E.P.Berdysheva@urfu.me <E.P.Berdysheva@urfu.me>
2022-12-23 23:17:45 +0500

    Задание 9.1 выполнено

--- Task91/Task91/Program.cs
+++ Task91/Task91/Program.cs
@@ -10,35 +10,23 @@ namespace Task91
     {
         static void Main(string[] args)
         {
-            using System;
-            using System.Collections.Generic;
-            using System.Linq;
-            using System.Text;
-            using System.Threading.Tasks;
-
-namespace Task91
-    {
-        internal class Program
-        {
-            static void Main(string[] args)
+            Console.WriteLine("Введите целое число, не равное 0");
+            int b = int.Parse(Console.ReadLine());
+            var check = true;
+            while (check)
             {
-                Console.WriteLine("Введите целое число, не равное 0");
-                int b = int.Parse(Console.ReadLine());
-                if (b == 0)
-                {
-                    Console.WriteLine("Неверное введенное значение");
-                    return;
-                }
-                int Summ = 0;
-                for (int i = -b; i <= b * b; i++)
-                {
-                    Summ += i * i;
-                }
-                Console.WriteLine(Summ);
+                check = b == 0;
+                if (!check)
+                    break;
+                Console.WriteLine("Введите верное значение");
+                b = int.Parse(Console.ReadLine());
             }
+            int summ = 0;
+            for (int i = -b; i <= b * b; i++)
+            {
+                summ += i * i;
+            }
+            Console.WriteLine($"Сумма квадратов всех целых чисел от -{b} до ({b} в квадрате) = {summ}");
         }
-    }
-
-}
     }
 }
