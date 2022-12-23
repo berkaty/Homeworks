4a1261f7d85946fa312b86478e3369de11c74d55
E.P.Berdysheva@urfu.me <E.P.Berdysheva@urfu.me>
2022-12-23 23:46:36 +0500

    Задание 9.4. выполнено

---
+++ Task9.4/Task9.4/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task94
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите натуральные числа а и b");
+            int a = int.Parse(Console.ReadLine());
+            int b = int.Parse(Console.ReadLine());
+            int k = 0;
+            var check = true;
+            while (check)
+            {
+                check = a >= b;
+                if (!check)
+                    break;
+                Console.WriteLine("Введите верное значение");
+                a = int.Parse(Console.ReadLine());
+                b = int.Parse(Console.ReadLine());
+            }
+            for (int i = a; i <= b; i++)
+            {
+                for (int j = 1; j <= i; j++)
+                {
+                    if ((i % j) == 0)
+                        if ((k + 1) >= 2)
+                        {
+                            Console.WriteLine(i);
+                            break;
+                        }
+                        else
+                            k += 1;
+                }
+            }
+        }
+    }
+}
