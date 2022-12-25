75d1c5bd7bf55aca385062e78e2f2628c0838aff
Бердышева Екатерина Павловна <102163481+berkaty@users.noreply.github.com>
2022-12-25 12:57:55 +0500

    Update Program.cs

--- Task92/Task92/Program.cs
+++ Task92/Task92/Program.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -12,20 +12,20 @@ namespace Task92
         {
             Console.WriteLine("Введите вещественное число, большее 1");
             double a = double.Parse(Console.ReadLine());
-            var check = true;
-            while (check)
-            {
-                check = a <= 1;
-                if (!check)
-                    break;
-                Console.WriteLine("Введите верное значение");
-                a = double.Parse(Console.ReadLine());
-            }
+
+            if (a<=1)
+                do
+                {
+                    Console.WriteLine("Введите верное значение");
+                    a = double.Parse(Console.ReadLine());
+                } while (a <= 1);
 
             double summ = 0;
             var number = 0;
+            int n = 1;
+            var i = true;
 
-            for (int n = 1; n < 10000; n++)
+            do
             {
                 summ += (1 / Math.Sqrt(n));
                 if (summ > a)
@@ -33,7 +33,9 @@ namespace Task92
                     number = n;
                     break;
                 }
-            }
+                n += 1;
+            } while (i);
+
             Console.WriteLine($"Наименьшее число n={number}");
         }
     }
